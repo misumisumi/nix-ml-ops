1318a9742d00a076a2ef290c8850acd334fe58cd
Yang, Bo <bo.yang@protonbase.io>
2024-03-27 01:09:04 +0000

    Put CUDA packages to layers

--- flake-modules/cuda.nix
+++ flake-modules/cuda.nix
@@ -14,35 +14,59 @@ topLevel@{ flake-parts-lib, inputs, ... }: {
         nixpkgs.config.allowUnfree = true;
         nixpkgs.config.cudaSupport = true;
 
-        ml-ops.common = { config, ... }: {
+        ml-ops.common = common: {
           config.LD_LIBRARY_PATH = lib.mkMerge [
             "/run/opengl-driver/lib"
             # bitsandbytes need to search for CUDA libraries
-            "${config.environmentVariables.CUDA_HOME}/lib"
+            "${common.config.environmentVariables.CUDA_HOME}/lib"
           ];
           config.devenvShellModule.packages = [
-            config.cuda.home
+            common.config.cuda.home
           ];
 
-          config.environmentVariables.CUDA_HOME = toString (config.cuda.home);
+          config.environmentVariables.CUDA_HOME = toString (common.config.cuda.home);
           options.cuda.home = lib.mkOption {
             type = lib.types.package;
             default = pkgs.symlinkJoin {
               name = "cuda-home";
-              paths = config.cuda.packages;
+              paths = common.config.cuda.packages;
             };
           };
           options.cuda.packages = lib.mkOption {
             type = lib.types.listOf lib.types.package;
           };
           config.cuda.packages = [
+            # TODO: Figure out if we can use `pkgs.cudaPackages.cuda_nvcc.lib` instead of `pkgs.cudaPackages.cuda_nvcc`. The `.lib` one is smaller.
             pkgs.cudaPackages.cuda_nvcc
+
+            # TODO: Remove `pkgs.cudaPackages.cudatoolkit` in favor of fine-grained packages.
             pkgs.cudaPackages.cudatoolkit
+
+            # TODO: Figure out if we can use `pkgs.cudaPackages.cuda_cudart.lib` instead of `pkgs.cudaPackages.cuda_cudart`. The `.lib` one is smaller.
             pkgs.cudaPackages.cuda_cudart
+
+            # TODO: Figure out if we can use `pkgs.cudaPackages.libcublas.lib` instead of `pkgs.cudaPackages.libcublas`. The `.lib` one is smaller.
             pkgs.cudaPackages.libcublas
+
             pkgs.cudaPackages.nccl
+
+            # TODO: Figure out if we can use `pkgs.cudaPackages.cudnn.lib` instead of `pkgs.cudaPackages.cudnn`. The `.lib` one is smaller.
             pkgs.cudaPackages.cudnn
           ];
+          config.devenvShellModule.containers.processes.layers =
+            (
+              builtins.foldl'
+                (layers: cudaPackage: layers ++ [
+                  (inputs.nix2container.packages.${system}.nix2container.buildLayer {
+                    deps = [
+                      cudaPackage
+                    ];
+                    inherit layers;
+                  })
+                ])
+                [ ]
+                common.config.cuda.packages
+            );
         };
       };
     });
