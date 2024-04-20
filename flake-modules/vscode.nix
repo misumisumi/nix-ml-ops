e0c67b3b50caa5c2a8fe8cc54400b287b7343ad2
Yang, Bo <bo.yang@protonbase.io>
2024-04-20 00:01:14 +0000

    Generate `.gitattributes`

--- flake-modules/vscode.nix
+++ flake-modules/vscode.nix
@@ -7,50 +7,50 @@ topLevel@{ flake-parts-lib, lib, ... }: {
     imports = [
       topLevel.config.flake.flakeModules.devcontainer
     ];
-    options.perSystem = flake-parts-lib.mkPerSystemOption {
-      config.ml-ops.devcontainer.nixago.requests = {
-        options = {
-          ".vscode/extensions.json".data.recommendations = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [ ];
-          };
-          ".vscode/settings.json" = {
-            data = lib.mkOption {
-              type = lib.types.attrsOf lib.types.anything;
-              default = { };
+    options.perSystem =
+      let
+        settingsJson = ".vscode/settings.json";
+
+        mkRecursiveDefault = value:
+          if builtins.isList value
+          then builtins.map mkRecursiveDefault value
+          else if builtins.isAttrs value
+          then lib.attrsets.mapAttrs (name: mkRecursiveDefault) value
+          else lib.mkDefault value;
+      in
+      flake-parts-lib.mkPerSystemOption {
+        config.ml-ops.devcontainer.nixago.copiedFiles = [
+          settingsJson
+        ];
+        config.ml-ops.devcontainer.nixago.requests = {
+          options = {
+            ".vscode/extensions.json".data.recommendations = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [ ];
             };
-            hook.mode = lib.mkOption {
-              type = lib.types.str;
-              default = "copy";
+            ".vscode/settings.json" = {
+              data = lib.mkOption {
+                type = lib.types.attrsOf lib.types.anything;
+                default = { };
+              };
             };
           };
+
+          config =
+            {
+              "${settingsJson}".data =
+                if builtins.pathExists "${flakeModule.self}/${settingsJson}"
+                then mkRecursiveDefault (builtins.fromJSON (builtins.readFile "${flakeModule.self}/${settingsJson}"))
+                else { }
+              ;
+              ".vscode/extensions.json".data = {
+                "recommendations" = [
+                  "mkhl.direnv"
+                ];
+              };
+
+            };
         };
-
-        config =
-          let
-            settingsJson = ".vscode/settings.json";
-
-            mkRecursiveDefault = value:
-              if builtins.isList value
-              then builtins.map mkRecursiveDefault value
-              else if builtins.isAttrs value
-              then lib.attrsets.mapAttrs (name: mkRecursiveDefault) value
-              else lib.mkDefault value;
-          in
-          {
-            "${settingsJson}".data =
-              if builtins.pathExists "${flakeModule.self}/${settingsJson}"
-              then mkRecursiveDefault (builtins.fromJSON (builtins.readFile "${flakeModule.self}/${settingsJson}"))
-              else { }
-            ;
-            ".vscode/extensions.json".data = {
-              "recommendations" = [
-                "mkhl.direnv"
-              ];
-            };
-
-          };
       };
-    };
   };
 }
