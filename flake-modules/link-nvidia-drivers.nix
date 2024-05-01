1a9b087943f252f0af81e31ddf1f900e6808bc0b
Bo Yang <bo.yang@protonbase.io>
2024-05-01 13:33:15 +0800

    Add a `link-nvidia-drivers`, which can be executed manually

--- flake-modules/link-nvidia-drivers.nix
+++ flake-modules/link-nvidia-drivers.nix
@@ -9,6 +9,31 @@ topLevel@{ flake-parts-lib, inputs, ... }: {
     ];
     options.perSystem = flake-parts-lib.mkPerSystemOption ({ lib, config, pkgs, options, ... }: {
       ml-ops.common = lib.attrsets.optionalAttrs pkgs.stdenv.isLinux {
+        devenvShellModule.scripts.link-nvidia-drivers.exec = ''
+          # Create the symbolic links to drivers when running the container
+          # with `nvidia-docker --gpus=all`
+          mkdir -p /run/opengl-driver/lib
+
+          (
+            shopt -s nullglob
+
+            # Note that nvidia-docker will mounts drivers to different
+            # directories according to the Linux distribution. For example,
+            # on Alpine Linux it's under /usr/lib64/ and on Debian it's
+            # under /usr/lib/x86_64-linux-gnu.
+            if \
+              ! ln -sf -t /run/opengl-driver/lib \
+                /usr/lib/x86_64-linux-gnu/libnvidia* \
+                /usr/lib/x86_64-linux-gnu/libcuda* \
+                /usr/lib64/libnvidia* \
+                /usr/lib64/libcuda* \
+                /usr/local/nvidia/lib64/libnvidia* \
+                /usr/local/nvidia/lib64/libcuda*
+            then
+              >&2 echo "Cannot find Nvidia drivers when trying to create symbolic links to Nvidia drivers"
+            fi
+          )
+        '';
         devenvShellModule.enterShell = ''
           # Link drivers if the script is either known to be in a container, or
           # it cannot detect if it is in a container
@@ -22,29 +47,7 @@ topLevel@{ flake-parts-lib, inputs, ... }: {
               >&2 echo "Trying to link Nvidia drivers to /run/opengl-driver/lib because we are in a devcontainer..."
             )
           then
-            # Create the symbolic links to drivers when running the container
-            # with `nvidia-docker --gpus=all`
-            mkdir -p /run/opengl-driver/lib
-
-            (
-              shopt -s nullglob
-
-              # Note that nvidia-docker will mounts drivers to different
-              # directories according to the Linux distribution. For example,
-              # on Alpine Linux it's under /usr/lib64/ and on Debian it's
-              # under /usr/lib/x86_64-linux-gnu.
-              if \
-                ! ln -sf -t /run/opengl-driver/lib \
-                  /usr/lib/x86_64-linux-gnu/libnvidia* \
-                  /usr/lib/x86_64-linux-gnu/libcuda* \
-                  /usr/lib64/libnvidia* \
-                  /usr/lib64/libcuda* \
-                  /usr/local/nvidia/lib64/libnvidia* \
-                  /usr/local/nvidia/lib64/libcuda*
-              then
-                >&2 echo "Cannot find Nvidia drivers when trying to create symbolic links to Nvidia drivers"
-              fi
-            )
+            link-nvidia-drivers
           fi
         '';
       };
